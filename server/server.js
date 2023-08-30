d0d213ac869a89685d9be074d3fb3acd1fbafca0
tawseefnabi9 <tawseef_bhat@discovery.com>
2023-08-30 17:04:05 +0530

    adding new feat

--- server/server.js
+++ server/server.js
@@ -1,36 +1,72 @@
-const express = require('express');
+import express  from 'express';
+import { pipeline } from '@xenova/transformers';
+import cors from 'cors'
 const app = express();
-const cors = require('cors');
+
 app.use(cors());
 app.use(express.json());
 
 let reviews = [];
 
 app.post('/api/add-review', (req, res) => {
-  console.log("er", req.body);
   const review = {
-    id: Date.now(),
+    id: reviews.length == 0 ? 100:  100+reviews.length+1,
     text: req.body.text,
     dateTime: new Date().toLocaleString(),
     upvotes: 0,
     downvotes: 0,
+    sentiment: {
+      'label': '',
+      'score': 0
+    }
   };
   reviews.push(review);
   res.status(201).json(review);
 });
+const handleAnalyze = async (reviews) => {
+  try {
+      let classifier = await pipeline('sentiment-analysis');
+      for(let r of reviews){
+        let sentiment = await classifier(r.text);
+        console.log("sentiments", sentiment);
+        r.sentiment['label'] = sentiment[0].label
+        r.sentiment['score'] = sentiment[0].score
+      }
+    return reviews
+  } catch (error) {
+    console.error('Error analyzing sentiment:', error);
+  }
+};
+app.post('/api/update-vote', (req, res) =>{
+  let {reviewId, voteType}  = req.body
+  let  _reviews = reviews.map(r =>{
+    if(r.id === reviewId){
+      if(voteType == 'up'){
+        r.upvotes += 1
+      } else if (voteType == 'down'){
+          r.downvotes += -1
+               
+      }
+    }
+  })
+  const sortedReviews = reviews.sort(
+    (a, b) => b.upvotes - b.downvotes - (a.upvotes - a.downvotes)
+  );
+  return  res.status(201).json(sortedReviews);
+})
 
 app.get('/api/get-reviews', (req, res) => {
   const sortedReviews = reviews.sort(
     (a, b) => b.upvotes - b.downvotes - (a.upvotes - a.downvotes)
   );
-  console.log("===", sortedReviews);
   res.json(sortedReviews);
 });
-app.get('/api/analyze-reviews', (req, res) => {
-  const sortedReviews = reviews.sort(
+app.get('/api/analyze-reviews', async (req, res) => {
+  let list = await handleAnalyze(reviews)
+  const sortedReviews = list.sort(
     (a, b) => b.upvotes - b.downvotes - (a.upvotes - a.downvotes)
   );
-  console.log("===", sortedReviews);
+    
   res.json(sortedReviews);
 });
 
